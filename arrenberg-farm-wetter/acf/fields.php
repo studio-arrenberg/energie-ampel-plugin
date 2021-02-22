7328aa1b0e9dcc4c763fde28dd7bee2eb90bbc06
j0hannr <52762073+j0hannr@users.noreply.github.com>
2021-02-22 17:25:49 +0100

    Add files via upload

---
+++ arrenberg-farm-wetter/acf/fields.php
@@ -0,0 +1,50 @@
+<?php
+
+// if( function_exists('acf_add_local_field_group') ):
+
+// 	acf_add_local_field_group(array(
+// 		'key' => 'group_602f93914132e',
+// 		'title' => 'Link-Block',
+// 		'fields' => array(
+// 			array(
+// 				'key' => 'field_602f9399a7cfc',
+// 				'label' => 'link',
+// 				'name' => 'link',
+// 				'type' => 'page_link',
+// 				'instructions' => '',
+// 				'required' => 0,
+// 				'conditional_logic' => 0,
+// 				'wrapper' => array(
+// 					'width' => '',
+// 					'class' => '',
+// 					'id' => '',
+// 				),
+// 				'post_type' => '',
+// 				'taxonomy' => '',
+// 				'allow_null' => 0,
+// 				'allow_archives' => 1,
+// 				'multiple' => 0,
+// 			),
+// 		),
+// 		'location' => array(
+// 			array(
+// 				array(
+// 					'param' => 'block',
+// 					'operator' => '==',
+// 					'value' => 'acf/link',
+// 				),
+// 			),
+// 		),
+// 		'menu_order' => 0,
+// 		'position' => 'normal',
+// 		'style' => 'default',
+// 		'label_placement' => 'top',
+// 		'instruction_placement' => 'label',
+// 		'hide_on_screen' => '',
+// 		'active' => true,
+// 		'description' => '',
+// 	));
+	
+// 	endif;
+
+?>
---
+++ arrenberg-farm-wetter/readme.md
@@ -0,0 +1,2 @@
+## Wordpress Plugin. Arrenberg Farm Wetter
+Plugin to display the Arrenberg Farm weatherprovided through Field Climate.
